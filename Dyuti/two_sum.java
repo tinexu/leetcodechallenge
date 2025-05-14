12883df51ff249ebc83380261dad5f0190217cba
Dyuti Phaneesh <100035555+dyuti-phaneesh@users.noreply.github.com>
2025-05-13 22:19:38 -0500

    ⭐️ two sum solution - dyuti

---
+++ Dyuti/two_sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int indexes = [2];
+        for (int i = 0; i < nums.length; i++) {
+            int tracker = target - nums[i];
+            if (map.containsKey(tracker)) {
+                indexes = {map.get(tracker), i}
+            }
+            map.put(nums[i], i);
+        }
+        return indexes;
+    }
+}
