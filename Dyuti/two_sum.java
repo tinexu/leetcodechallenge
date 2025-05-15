87d16056323d8d5a9b3e700a8d3e5d4bfd4dae36
Dyuti Phaneesh <100035555+dyuti-phaneesh@users.noreply.github.com>
2025-05-15 17:03:47 -0500

    forgot how java works 😅

--- Dyuti/two_sum.java
+++ Dyuti/two_sum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int indexes = new int[2];
+        int[] indexes = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int tracker = target - nums[i];
             if (map.containsKey(tracker)) {
-                indexes = {map.get(tracker), i};
-                break;
+                indexes[0] = map.get(tracker);
+                indexes[1] = i;
             }
             map.put(nums[i], i);
         }
