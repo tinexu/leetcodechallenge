f3c86093d1d473903727490624f809da0619313b
tinexu <121458001+tinexu@users.noreply.github.com>
2025-05-17 16:00:46 -0500

    Create maxsubarray.java

---
+++ Christine/maxsubarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int maxEnding = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            
+            maxEnding = Math.max(maxEnding + nums[i], nums[i]);
+          
+            maxSum = Math.max(maxSum, maxEnding);
+        }
+
+        return maxSum;
+    }
+}
