ffaa589404bf60b1778f8c106b0e02241dd63521
RithvikKrishnan <70037200+RithvikKrishnan@users.noreply.github.com>
2025-05-21 14:29:56 -0400

    Create ValidAnagram.py

---
+++ RIthvik/ValidAnagram.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        charsT = {}
+        charsS = {}
+        for character in string.printable:
+            charsS[character] = 0
+            charsT[character] = 0
+        for i in range(len(s)):
+            charsS[s[i]] += 1
+            charsT[t[i]] += 1
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        for character in letters:
+            if charsS[character] != charsT[character]:
+                return False
+        return True
