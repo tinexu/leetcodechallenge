fa59da6a598bf6ba9b80195ab63c27e45aece36e
tinexu <121458001+tinexu@users.noreply.github.com>
2025-06-15 13:36:33 -0500

    Create MinimumSizeSubarraySum.java

---
+++ Christine/June/MinimumSizeSubarraySum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                min = (min < right - left + 1 ? min : right - left + 1);
+                sum -= nums[left];
+
+                left++;
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
