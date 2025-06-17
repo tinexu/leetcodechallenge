1f79a1355b5d4b764415382242650624790dd3c6
tinexu <121458001+tinexu@users.noreply.github.com>
2025-06-16 22:38:34 -0500

    Create ValidParantheses.java

---
+++ Christine/June/ValidParantheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        if (s.length() == 1) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) == '(') || (s.charAt(i) == '{') || (s.charAt(i) == '[')) {
+                stack.push(s.charAt(i));
+            }
+            else {
+                if (s.charAt(i) == ')') {
+                    if ((stack.isEmpty()) || (stack.pop() != '(')) {
+                        return false;
+                    }
+                }
+                else if (s.charAt(i) == '}') {
+                    if ((stack.isEmpty()) || (stack.pop() != '{')) {
+                        return false;
+                    }
+                }
+                else {
+                    if ((stack.isEmpty()) || (stack.pop() != '[')) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
